f74fc74a550639f977acd6027a96cb756317071e
TSMERORR <101246625+TSMERORR@users.noreply.github.com>
2024-10-09 19:26:55 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,16 @@ require('dotenv').config();
 
 const setupServer = () => {
   const app = express();
+  const PORT = Number(env('PORT', 3000));
+
+    app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
   app.use(cors());
   app.use(express.json());
 
@@ -44,9 +54,12 @@ const setupServer = () => {
     }
   });
 
+   app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
 
-
-  const PORT = Number(env('PORT', 3000));
 
   app.listen(PORT, () => {
     pino.info(`Server is running on port ${PORT}`);
